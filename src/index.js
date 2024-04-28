67ae1ca7ea1216b61f1e17ef456042851633e2eb
kaziarman23 <kaziarman@proton.me>
2024-04-28 12:41:02 +0600

    mini project complite

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom/client";
+import  "../src/style.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const BookList = () => {
+    return (
+        <section className="section">
+            <Book/>
+            <Book/>
+            <Book/>
+        </section>
+    );
+};
+
+const Book = () => {
+    return (
+        <article className="article">
+            <Image/>
+            <Title/>
+            <Author/>
+        </article>
+    );
+};
+
+const Image = () =>(
+    <img src="https://images.unsplash.com/photo-1712650827891-cf87e3b8aaec?q=80&w=1539&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="an-img"
+    />
+)
+
+const Title = () => <h2>Intersting Facts For Curious Minds</h2>;
+const Author = () => {
+    return <h4>Jordan Moore</h4>
+}
+
+const root = ReactDom.createRoot(document.getElementById('root'));
+
+root.render(<BookList/>);
