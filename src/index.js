573515f58bb75e70189aa26dc3b2f36f8c354324
kaziarman23 <kaziarman@proton.me>
2024-04-28 13:17:17 +0600

    project runing well

--- src/index.js
+++ src/index.js
@@ -23,16 +23,15 @@ const Book = () => {
         </article>
     );
 };
-
 const Image = () =>(
     <img src="https://images.unsplash.com/photo-1712650827891-cf87e3b8aaec?q=80&w=1539&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="an-img"
     />
 )
-
 const Title = () => <h2>Intersting Facts For Curious Minds</h2>;
 const Author = () => {
     return <h4>Jordan Moore</h4>
 }
+
 
 const root = ReactDom.createRoot(document.getElementById('root'));
 
