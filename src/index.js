14be8c98d34c5a26dc48608b79c1099aa3b001be
kaziarman23 <kaziarman@proton.me>
2024-04-29 11:05:30 +0600

    applay variable in the project

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import  "../src/style.css";
 
-
+const img = "https://images.unsplash.com/photo-1712650827891-cf87e3b8aaec?q=80&w=1539&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+const title = "Intersting Facts For Curious Minds";
+const author = "Jordan Moore";
 
 const BookList = () => {
     return (
         <section className="section">
-            <Book/>
             <Book/>
             <Book/>
         </section>
@@ -17,20 +18,12 @@ const BookList = () => {
 const Book = () => {
     return (
         <article className="article">
-            <Image/>
-            <Title/>
-            <Author/>
+            <img src={img} alt={title} />
+            <h2>{title}</h2>
+            <h4>{author}</h4>            
         </article>
     );
 };
-const Image = () =>(
-    <img src="https://images.unsplash.com/photo-1712650827891-cf87e3b8aaec?q=80&w=1539&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="an-img"
-    />
-)
-const Title = () => <h2>Intersting Facts For Curious Minds</h2>;
-const Author = () => {
-    return <h4>Jordan Moore</h4>
-}
 
 
 const root = ReactDom.createRoot(document.getElementById('root'));
